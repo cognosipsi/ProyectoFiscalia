29ba6becd47f019ab6487d574ef92ec88297830f
MarleneManzanit <marlenelagospalacios@gmail.com>
2021-03-25 00:43:35 +0000

    Boolean agregado

--- ProyectoFiscalia.java
+++ ProyectoFiscalia.java
@@ -5,9 +5,10 @@ public class ProyectoFiscalia {
 
   public static void main(String[] args) {
      Procedimiento prueba;
-     ArrayList<String> part= new ArrayList<String>();
-     ArrayList<String> rol= new ArrayList<String>();
-     ArrayList<Causa> actual= new ArrayList<Causa>();
+     ArrayList<Procedimiento> peritaje = new ArrayList<Procedimiento>();
+     ArrayList<String> part = new ArrayList<String>();
+     ArrayList<String> rol = new ArrayList<String>();
+     ArrayList<Causa> actual = new ArrayList<Causa>();
      Causa caso;
      Fiscal nuevo;
      part.add("Juan Perez");
@@ -18,11 +19,12 @@ public class ProyectoFiscalia {
      prueba.setParticipantes(part);
      prueba.setRoles(rol);
      prueba.setResultado("Se hace una denuncia por estafa");
+     peritaje.add(prueba);
      caso.setCodigo("123456");
      caso.setEstado("Abierto");
      caso.setTipoCaso("Delitos economicos");
      caso.setRegion("VI");
-     caso.setPeritajes(prueba);
+     caso.setPeritajes(peritaje);
      actual.add(caso);
      nuevo.setNombre("Maria Castillo");
      nuevo.setRut("12345768-9");
@@ -34,14 +36,9 @@ public class ProyectoFiscalia {
 
   public class Procedimiento {
     private String nombreProc;
-    private ArrayList<String> participantes;
-    private ArrayList<String> roles;
+    private ArrayList<String> participantes=new ArrayList<String>();
+    private ArrayList<String> roles=new ArrayList<String>();
     private String resultado;
-
-    public Procedimiento(){
-      participantes= new ArrayList<String>();
-      roles=new ArrayList<String>();
-    }
 
     public String getNombreProc() {
       return nombreProc;
@@ -79,14 +76,12 @@ public class ProyectoFiscalia {
   public class Causa {
     private String codigo;
     private Fiscal encargado;
-    private ArrayList<Procedimiento> peritajes;
+    private ArrayList<Procedimiento> peritajes=new ArrayList<Procedimiento>();
     private String estado;
     private String tipoCaso;
     private String region;
+    private boolean asignada;
 
-    public Causa(){
-      peritajes= new ArrayList<Procedimiento>();
-    }
 
     public String getCodigo() {
       return codigo;
@@ -136,18 +131,23 @@ public class ProyectoFiscalia {
       this.region = region;
     }
 
+    public boolean getAsignada(){
+      return asignada;
+    }
+
+    public void setAsignada(boolean asignada){
+      this.asignada=asignada;
+    }
+
   }
 
   public class Fiscal {
     private String nombre;
     private String rut;
-    private ArrayList<Causa> causasActuales;
+    private ArrayList<Causa> causasActuales=new ArrayList<Causa>();
     private String especialidad;
     private String region;
 
-    public Fiscal(){
-      causasActuales=new ArrayList<Causa>();
-    }
 
     public String getNombre() {
       return nombre;
